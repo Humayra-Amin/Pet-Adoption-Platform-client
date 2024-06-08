221c92f215112b511da9d0d135b441841af01785
Humayra-Amin <humaema4576@gmail.com>
2024-06-08 18:14:11 +0600

    add pet form created and added to database

--- src/components/UserDashboard/Dashboard/Dashboard.jsx
+++ src/components/UserDashboard/Dashboard/Dashboard.jsx
@@ -1,55 +1,67 @@
 import { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
+import { FaBars, FaDonate } from 'react-icons/fa';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { MdOutlinePets } from "react-icons/md";
+import { BsCollectionPlay } from "react-icons/bs";
+import { FiHeart } from "react-icons/fi";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { BiDonateHeart } from "react-icons/bi";
 
 const Dashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     };
 
     return (
-        <div>
+        <div className="lg:flex">
             <button onClick={toggleSidebar} className="lg:hidden p-2 text-white bg-green-600">
-            <FaBars size={24}></FaBars>
+                <FaBars size={24}></FaBars>
             </button>
 
-            <aside className={`lg:block ${isOpen ? 'block' : 'hidden'} w-64 bg-green-600 text-white min-h-full lg:min-h-screen`}>
-                <div className="p-6">
-                    <nav>
+            <aside className={`lg:block ${isOpen ? 'block' : 'hidden'} w-64 bg-green-600 text-white min-h-screen`}>
+                <div className="p-6 flex flex-col h-full">
+                    <nav className="flex-1">
                         <ul className="space-y-6">
                             <div>
-                                <h1 className='border-2 bg-green-400 border-green-400 text-center text-xl playfair font-semibold p-1'>User Dashboard</h1>
+                                <Link to='/dashboard'><h1 className='border-2 bg-green-200 text-black border-green-200 text-center text-xl playfair font-semibold p-1 mt-16 rounded-lg shadow-lg'>User Dashboard</h1></Link>
                             </div>
                             <li>
-                                <NavLink to="/dashboard/add-pet" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    Add Pet
+                                <NavLink to="/dashboard/add-pet" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/add-pet' ? 'bg-green-400': 'hover:bg-green-900'}`}>
+                                    <MdOutlinePets />
+                                    <span className='mr-24'>Add Pet</span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/my-added-pets" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    My Added Pets
+                                <NavLink to="/dashboard/my-added-pets" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/my-added-pets' ? 'bg-green-400' : 'hover:bg-green-900'}`}>
+                                    <BsCollectionPlay />
+                                    <span className='mr-12'>My Added Pets</span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/adoption-request" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    Adoption Request
+                                <NavLink to="/dashboard/adoption-request" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/adoption-request' ? 'bg-green-400' : 'hover:bg-green-900'}`}>
+                                    <FiHeart />
+                                    <span className='mr-8'>Adoption Request</span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/create-donation-campaign" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    Create Donation Campaign
+                                <NavLink to="/dashboard/create-donation-campaign" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/create-donation-campaign' ? 'bg-green-400' : 'hover:bg-green-900'}`}>
+                                    <RiMoneyDollarCircleFill />
+                                    <span className='ml-4'>Create Donation Campaign</span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/my-donation-campaigns" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    My Donation Campaigns
+                                <NavLink to="/dashboard/my-donation-campaigns" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/my-donation-campaigns' ? 'bg-green-400' : 'hover:bg-green-900'}`}>
+                                    <BiDonateHeart />
+                                    <span className='ml-4'>My Donation Campaigns</span>
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/my-donations" className={({ isActive }) => isActive ? 'block p-2 bg-green-500 rounded' : 'block p-2 hover:bg-green-900 rounded'}>
-                                    My Donations
+                                <NavLink to="/dashboard/my-donations" className={`flex items-center justify-between p-2 rounded ${location.pathname === '/dashboard/my-donations' ? 'bg-green-400' : 'hover:bg-green-900'}`}>
+                                    <FaDonate />
+                                    <span className='mr-16'>My Donations</span>
                                 </NavLink>
                             </li>
                         </ul>
